3671767b59cb4bf3bce2fa95deb319e90ec3d75d
Chase Brown <chaseb686@gmail.com>
2018-05-10 19:46:48 -0600

    updated for single year / comments updated

--- main.py
+++ main.py
@@ -5,12 +5,25 @@ import pandas as pd
 if __name__ == '__main__':
 
     # Multiyear agg
-    basefile = 'Snow19902016/'
+    basefile = 'Multiyear/'
     df_precip, df_temp, df_snow = SnowCalc.agg_regions_snow(basefile, 26, '199001', '201512')
 
+    month = SnowCalc.precip_month(df_precip, df_snow)
+    temp_month = SnowCalc.temp_month(df_temp)
+
+    df_snow.drop(df_snow.columns[0:-14], axis=1, inplace=True)
+    df_snow.drop(df_snow.columns[12:], axis=1, inplace=True)
+
+    df = pd.concat([month, temp_month, df_snow], axis=1)
+    snow = df['snow']
+    df.drop(labels=['snow'], axis=1, inplace=True)
+    df['snow'] = snow
+
+    df.to_csv(basefile+'snowtotals.csv')
+
     # single year
-    #basefile = '2017/'
-    #df_precip, df_temp, df_snow = SnowCalc.agg_regions_snow(basefile, 1, '201701', '201712')
+    basefile = '2017/'
+    df_precip, df_temp, df_snow = SnowCalc.agg_regions_snow(basefile, 1, '201701', '201712')
 
     month = SnowCalc.precip_month(df_precip, df_snow)
     temp_month = SnowCalc.temp_month(df_temp)
